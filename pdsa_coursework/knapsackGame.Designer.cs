232ae34df3430af486f928efca9036d09404cce0
Malindu Pabasara <mpabasara11@gmail.com>
2022-11-10 14:26:58 +0530

    commin1
    added the calculation method to change it later

--- pdsa_coursework/knapsackGame.Designer.cs
+++ pdsa_coursework/knapsackGame.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // knapsackGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "knapsackGame";
             this.Text = "knapsackGame";
+            this.Load += new System.EventHandler(this.knapsackGame_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- pdsa_coursework/knapsackGame.cs
+++ pdsa_coursework/knapsackGame.cs
@@ -16,5 +16,67 @@ namespace pdsa_coursework
         {
             InitializeComponent();
         }
+
+        private void knapsackGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void calculate() 
+        {
+            // TODO Auto-generated method stub
+            System.out.println("Maximum profit for 0/1 Knapsack Problem");
+            System.out.println("Enter the number of items");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int[] weight = new int[n];
+            int[] profit = new int[n];
+            System.out.println("Enter the weight of each item");
+            for (int i = 0; i < n; i++)
+            {
+                weight[i] = sc.nextInt();
+            }
+            System.out.println("Enter the profit of each item");
+            for (int i = 0; i < n; i++)
+            {
+                profit[i] = sc.nextInt();
+            }
+            System.out.println("Enter the capacity of the knapsack");
+            int capacity = sc.nextInt();
+            int[][] matrix = new int[n + 1][capacity + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= capacity; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                    else if (weight[i - 1] <= j)
+                    {
+                        matrix[i][j] = Math.max(profit[i - 1] + matrix[i - 1][j - weight[i - 1]], matrix[i - 1][j]);
+                    }
+                    else
+                    {
+                        matrix[i][j] = matrix[i - 1][j];
+                    }
+                }
+            }
+            System.out.println("The maximum profit is " + matrix[n][capacity]);
+            System.out.println("The items selected are");
+            int i = n;
+            int j = capacity;
+            while (i > 0 && j > 0)
+            {
+                if (matrix[i][j] != matrix[i - 1][j])
+                {
+                    System.out.println("Item " + i + " with weight " + weight[i - 1] + " and profit " + profit[i - 1]);
+                    j = j - weight[i - 1];
+                }
+                i--;
+            }
+
+        }
     }
 }
