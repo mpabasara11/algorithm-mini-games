332b35cf9eae10857b9d565b98061ccfcf772775
Malindu Pabasara <mpabasara11@gmail.com>
2022-11-12 15:13:09 +0530

    algorithm completed

--- pdsa_coursework/knapsackGame.cs
+++ pdsa_coursework/knapsackGame.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace pdsa_coursework
 {
@@ -19,64 +20,99 @@ namespace pdsa_coursework
 
         private void knapsackGame_Load(object sender, EventArgs e)
         {
+        
 
-        }
+
+            int[] weight = new int[10];
+            int[] profit = new int[10];
+
+            //item name assign
+            String[] item = new string[10];
+            item[0] = "A";
+            item[1] = "B";
+            item[2] = "C";
+            item[3] = "D";
+            item[4] = "E";
+            item[5] = "F";
+            item[6] = "G";
+            item[7] = "H";
+            item[8] = "I";
+            item[9] = "J";
+       
 
 
-        public void calculate() 
-        {
-            // TODO Auto-generated method stub
-            System.out.println("Maximum profit for 0/1 Knapsack Problem");
-            System.out.println("Enter the number of items");
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            int[] weight = new int[n];
-            int[] profit = new int[n];
-            System.out.println("Enter the weight of each item");
-            for (int i = 0; i < n; i++)
+
+            Random rnd = new Random();
+
+            //random weight assign
+            for (int i = 0; i < 10; i++)
             {
-                weight[i] = sc.nextInt();
+                weight[i] = rnd.Next(1, 5);
             }
-            System.out.println("Enter the profit of each item");
-            for (int i = 0; i < n; i++)
+
+            //random profit assign
+            for (int i = 0; i < 10; i++)
             {
-                profit[i] = sc.nextInt();
+                profit[i] = rnd.Next(5, 10);
             }
-            System.out.println("Enter the capacity of the knapsack");
-            int capacity = sc.nextInt();
-            int[][] matrix = new int[n + 1][capacity + 1];
-            for (int i = 0; i <= n; i++)
+
+       
+
+
+            //set table headings
+            lstviewItems.Columns.Add("Item",50,HorizontalAlignment.Center);
+            lstviewItems.Columns.Add("Weight",150, HorizontalAlignment.Center);
+            lstviewItems.Columns.Add("Profit",150, HorizontalAlignment.Center);    
+
+
+            lstviewItems.BackColor = Color.Lavender;
+            lstviewItems.ForeColor = Color.Black;
+            lstviewItems.View = View.Details;
+
+
+            //fill table data
+            for (int i = 0; i < 10; i++) 
+            {
+                lstviewItems.Items.Add(item[i]);
+                lstviewItems.Items[i].SubItems.Add(weight[i].ToString());
+                lstviewItems.Items[i].SubItems.Add(profit[i].ToString());
+            }
+            //////////////////////////////////////
+           
+            //knapsack max weight assign
+            int capacity = 10;
+
+            int[,] matrix = new int[10 + 1,capacity+1];
+
+
+            for (int i = 0; i <= 10; i++)
             {
                 for (int j = 0; j <= capacity; j++)
                 {
                     if (i == 0 || j == 0)
                     {
-                        matrix[i][j] = 0;
+                        matrix[i,j] = 0;
                     }
                     else if (weight[i - 1] <= j)
                     {
-                        matrix[i][j] = Math.max(profit[i - 1] + matrix[i - 1][j - weight[i - 1]], matrix[i - 1][j]);
+                        matrix[i,j] = Math.Max(profit[i - 1] + matrix[i - 1,j - weight[i - 1]], matrix[i - 1,j]);
                     }
                     else
                     {
-                        matrix[i][j] = matrix[i - 1][j];
+                        matrix[i,j] = matrix[i - 1,j];
                     }
                 }
             }
-            System.out.println("The maximum profit is " + matrix[n][capacity]);
-            System.out.println("The items selected are");
-            int i = n;
-            int j = capacity;
-            while (i > 0 && j > 0)
-            {
-                if (matrix[i][j] != matrix[i - 1][j])
-                {
-                    System.out.println("Item " + i + " with weight " + weight[i - 1] + " and profit " + profit[i - 1]);
-                    j = j - weight[i - 1];
-                }
-                i--;
-            }
+
+            label1.Text= matrix[10,capacity].ToString();
+
+
+            //////////////////////////////
+
 
         }
+
+
+      
     }
 }
